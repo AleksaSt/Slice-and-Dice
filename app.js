799c1228a7f9ebb72e7b0af2611b9c3d8ef47294
AleksaSt <alexastojcevic@yahoo.com>
2021-11-22 18:16:10 +0100

    fixed carousel and added css

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonials-item");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
   }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("testimonials-item");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+
+  function myFunction(x) {
+    if (x.matches) { 
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    } else {
+      slides[slideIndex-1].style.display = "flex";  
+      dots[slideIndex-1].className += " active";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "flex";  
-    dots[slideIndex-1].className += " active";
   }
+  
+  var x = window.matchMedia("(max-width: 768px)")
+  myFunction(x) 
+  x.addListener(myFunction) // Attach listener function on state changes
+}
+
+/* Nav Toggle */
+let nav = $("#nav");
+let navToggle =$("#navToggle");
+
+$("#navToggle").on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
+});
+
+
+
+
+
 
